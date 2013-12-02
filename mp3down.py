a7c017890f6e679a14e5607c430b7493e905ea91
ruri <l99elele@gmail.com>
2013-12-02 11:03:16 +0900

    no message

--- mp3down.py
+++ mp3down.py
@@ -1,19 +1,21 @@
-import re, httplib, urllib2, os, sys
+import re, httplib, urllib2, os, sys, datetime, time
 
-class down:
+class mp3down:
 	conn = httplib.HTTPConnection("mp3.zing.vn") 
 	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/html"} 
-
-	def __init__(self): 
-		pass
+	logFolder = "./log/"
+	def __init__(self):  
+		if not os.path.exists(os.path.dirname(self.logFolder)):
+			os.makedirs(os.path.dirname(self.logFolder)) 
 
 	def download(self, artist):  
+		self.logFile = open(self.logFolder + str(datetime.date.today()) + '.log', 'ab')	
 		cnt = 1 
-		for t in range(1,100):
+		for page in range(1,100):
 			try:  
-				self.conn.request("POST", "/nghe-si/"+artist+"/bai-hat?p="+str(t), "", self.headers)   
-				r = self.conn.getresponse()   
-				result = r.read()   
+				self.conn.request("POST", "/nghe-si/"+artist+"/bai-hat?p="+str(page), "", self.headers)   
+				resonse = self.conn.getresponse()   
+				result = resonse.read()   
 				ff2 = re.findall('class="music-download _btnDownload" href="[^"]*"', result)  
 				for ii in ff2:
 					ii = ii.replace('class=\"music-download _btnDownload\" href=\"','')
@@ -29,12 +31,14 @@ class down:
 					file_size = int(meta.getheaders("Content-Length")[0])
 								  
 					if os.path.exists(file_name) and os.path.getsize(os.path.abspath(file_name)) >= file_size:
-						print "Already exists file : %s" % (file_name)
+						msg = "Already exists file : %s" % (file_name) 
+						print msg
+						self.log(msg)
 						continue
 
 					f = open(file_name, 'wb')
 
-					print "Downloading: %s Bytes: %s" % (file_name, file_size)
+					print "Downloading\t%s\t%s" % (file_name, file_size)
 					file_size_dl = 0
 					block_sz = 81920
 					while True:
@@ -53,32 +57,41 @@ class down:
 						else:
 							print status
 
+					self.log(str(cnt)+'\t'+str(file_size)+'\t'+file_name)
+
 					f.close()  
 					cnt  = cnt + 1 
 			except:
-				print "Error"
+				print "error"
+				self.log("error")
+				f.close()  
 				continue
+
+		self.logFile.close()
 	
 	def getArtist(self):
-		artist = ""
+		artist = "" 
 		while artist == "":
 			artist = raw_input('Artist : ').replace(' ','+')
 
-		self.conn.request("GET", "/tim-kiem/bai-hat.html?q="+artist, "", self.headers)   
-		r = self.conn.getresponse()   
-		result = r.read()    
-		ff = re.findall('<a href="/nghe-si/[^"]*"', result) 
-		if len(ff) > 0: 
-			artist = ff[0].replace('<a href="/nghe-si/','')
-			artist = artist.replace('"','')
-		
+			self.conn.request("GET", "/tim-kiem/bai-hat.html?q="+artist, "", self.headers)   
+			result = self.conn.getresponse().read()   
+			arry = re.findall('<a href="/nghe-si/[^"]*"', result) 
+			if len(arry) > 0: 
+				artist = (arry[0].replace('<a href="/nghe-si/','')).replace('"','')
+			else:
+				print "'",artist,"' Does not exists"
+				artist = ""
+
 		return artist
-
 	
+	def log(self, msg):
+		t = time.localtime()
+		self.logFile.write(self.strfill(t.tm_hour,2,'0') + ":" + self.strfill(t.tm_min,2,'0') + ":" + self.strfill(t.tm_sec,2,'0') + "\t" + msg + '\r\n') 
+	
+	def strfill(self, s, l, f):
+		return str(f)*(l-len(str(s)))+str(s)
 		
-	
 
-
-
-d = down()
+d = mp3down()
 d.download(d.getArtist())
